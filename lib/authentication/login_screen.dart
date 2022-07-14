78c025da893f2243e47559195c348c62b0e20f66
unknown <kusumsaru833@gmail.com>
2022-07-14 12:30:36 +0545

    loginpage

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  LoginScreen({Key? key}) : super(key: key);
-
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
@@ -10,6 +8,28 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: Image.asset("assets/image.jpg"),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "Login as a Owner",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
