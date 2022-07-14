cc1f2ae874b3bc048253bd1cb1ca4c6610695717
unknown <kusumsaru833@gmail.com>
2022-07-14 21:35:07 +0545

    backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rentomatic_app/splashScreen/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(
     MyApp(
       child: MaterialApp(
